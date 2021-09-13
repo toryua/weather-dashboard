85f747b00d55c435325abada606b60b6565aac49
toryua <87789457+toryua@users.noreply.github.com>
2021-09-13 13:20:31 -0700

    most recent code fetch attempt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,37 @@
-var searchBtnEl = document.getElementById('#searchBtnEl');
-var cityNameEl = document.getElementById('#city');
-var city = cityNameEl.value; 
+var searchBtnEl = document.getElementById('searchBtn');
+var cityNameEl = document.getElementById('city');
+var cityEl = "Phoenix";
+ 
 
-
-
-
-var weatherSearch = 
-    fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q={" +
-        city + "}&appid={" +
-          "4905268bbaa813b64a0dd03408e9b4eb}"
-      );
+function weatherSearch () {
+    console.log("weatherSearch is called!");
     
-      
-    
+    fetch('https://api.openweathermap.org/data/2.5/find?q=' +
+        cityEl + '&units=imperial&appid=' + '4905268bbaa813b64a0dd03408e9b4eb'
+      )
+      .then(function(response) {
+          return response.json()
+      })
+      .then(function(myJson) {
+          console.log(JSON.stringify(myJson));
+          var weatherResponse = document.querySelector('#weather-container')
+          weatherResponse.innerHTML= cityEl + Date() + myJson.main.temp + myJson.wind.speed + myJson.humidity.value
+          ;
+      });
+      console.log("fetch is finished!"); 
 
+      /*function weatherSearch() {
+        let x = document.forms["myForm"]["fname"].value;
+        fetch('https://api.openweathermap.org/data/2.5/find?q=' +
+              x + '&units=imperial&appid=' + '4905268bbaa813b64a0dd03408e9b4eb'
+            )
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function(myJson) {
+                console.log(JSON.stringify(myJson));
+        }); 
+    }; */
 
-
-
-
-document.addEventListener("click", weatherSearch);
+}         
+searchBtnEl.addEventListener("click", weatherSearch())
