e9d24244098813e17d0e1875a257e74a06545886
toryua <87789457+toryua@users.noreply.github.com>
2021-09-13 19:56:13 -0700

    fetch update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var weatherForm = document.getElementById('myForm');
-​
 function weatherSearch (event) {
     event.preventDefault();
     var cityNameEl = document.getElementById("city").value;
@@ -7,30 +6,23 @@ function weatherSearch (event) {
         console.log(cityEl + " is the city name");
     console.log("weatherSearch is called!");
     console.log(cityEl);
-    fetch("https://api.openweathermap.org/data/2.5/find?q=" + cityEl + "&units=imperial&appid=" + "4905268bbaa813b64a0dd03408e9b4eb"
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&units=imperial&appid=" + "4905268bbaa813b64a0dd03408e9b4eb"
       )
       .then(function(response) {
           return response.json()
       })
       .then(function(myJson) {
-          console.log(JSON.stringify(myJson));
+          console.log(myJson);
           var weatherResponse = document.querySelector('#weather-container')
-          weatherResponse.innerHTML= cityEl + " weather report for " + Date() // +  myJson.main.temp + myJson.wind.speed + myJson.humidity.value
+          weatherResponse.innerHTML= cityEl + " weather report for " + Date() + ": " +  myJson.main.temp + myJson.wind.speed + myJson.humidity.value
           ;
       });
-      console.log("fetch is finished!"); 
-      /*function weatherSearch() {
-        let x = document.forms["myForm"]["fname"].value;
-        fetch('https://api.openweathermap.org/data/2.5/find?q=' +
-              x + '&units=imperial&appid=' + '4905268bbaa813b64a0dd03408e9b4eb'
-            )
-            .then(function(response) {
-                return response.json()
-            })
-            .then(function(myJson) {
-                console.log(JSON.stringify(myJson));
-        }); 
-    }; */
-​
-}         
+ 
+      localStorage.setItem('citySearch', cityEl);  
+    
+}     
+
+var citySearch = document.getElementById('previous-searches');
+citySearch.innerHTML = localStorage.getItem('citySearch');
+
 weatherForm.addEventListener("submit", weatherSearch);
